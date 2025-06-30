1ff0ceae338d38c722fc334533276d70478a333c
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-06-30 16:52:02 +0600

    login form created for login user

--- src/components/modules/Auth/Login/LoginForm.tsx
+++ src/components/modules/Auth/Login/LoginForm.tsx
@@ -70,94 +70,92 @@ export default function LoginForm() {
     setValue("password", "123456Aa!");
   };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
-      <div className="grid grid-cols-1 lg:grid-cols-2 w-full max-w-screen-lg rounded-xl overflow-hidden shadow-xl">
-        <div>
-          <Card className="w-full md:p-6">
-            <CardHeader>
-              <CardTitle className=" text-xl lg:text-2xl font-bold text-center">
-                Login your account
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <FormProvider {...form}>
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="space-y-4"
+    <div className="min-h-screen h-screen w-full max-w-3xl flex items-center justify-center  p-4 mx-auto">
+      <div className="w-full md:p-6">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="text-xl lg:text-2xl font-bold text-center">
+              Login your account
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <FormProvider {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="identifier"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email or Phone Number</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder="Enter your email or phone number"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type="password"
+                          placeholder="Enter your password"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <button
+                  type="submit"
+                  className="hover:cursor-pointer border border-neutral-300 px-4 flex py-[6px] gap-3 items-center justify-center font-medium rounded-md transition-all duration-300 ease-in-out hover:bg-indigo-500 hover:text-white my-4 mt-2 w-full"
                 >
-                  <FormField
-                    control={form.control}
-                    name="identifier"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email or Phone Number</FormLabel>
-                        <FormControl>
-                          <Input
-                            {...field}
-                            placeholder="Enter your email or phone number"
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                          <Input
-                            {...field}
-                            type="password"
-                            placeholder="Enter your password"
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <button
-                    type="submit"
-                    className="hover:cursor-pointer border border-neutral-300 px-4 flex py-[6px] gap-3 items-center justify-center font-medium rounded-md transition-all duration-300 ease-in-out hover:bg-teal-700 hover:text-white  my-4 mt-2 w-full"
-                  >
-                    {isSubmitting ? "Logging..." : "Login"}
-                  </button>
-                </form>
-                <p className="text-center text-sm text-gray-600 my-5">
-                  Don&apos;t have an account?{" "}
-                  <Link
-                    href="/register"
-                    className="text-blue-600 hover:underline"
-                  >
-                    Sign up
-                  </Link>
-                </p>
-                <Separator />
-                <p className="text-center text-gray-500 text-[15px] my-5">
-                  Click here to auto-fill your email and password
-                </p>
-                <div className="flex flex-col gap-4 md:flex-row">
-                  <button
-                    onClick={handleUserAutoFillButton}
-                    type="submit"
-                    className="hover:cursor-pointer border border-neutral-300 px-4 flex py-[6px] gap-3 items-center justify-center font-medium rounded-full transition-all duration-300 ease-in-out hover:bg-teal-700 hover:text-white  my-4 mt-2 w-full flex-1  bg-zinc-50"
-                  >
-                    User
-                  </button>
-                  <button
-                    onClick={handleAdminAutoFillButton}
-                    type="submit"
-                    className="hover:cursor-pointer border border-neutral-300 px-4 flex py-[6px] gap-3 items-center justify-center font-medium rounded-full transition-all duration-300 ease-in-out hover:bg-teal-700 hover:text-white  my-4 mt-2 w-full flex-1 bg-zinc-50"
-                  >
-                    Admin
-                  </button>
-                </div>
-              </FormProvider>
-            </CardContent>
-          </Card>
-        </div>
+                  {isSubmitting ? "Logging..." : "Login"}
+                </button>
+              </form>
+              <p className="text-center text-sm text-gray-600 my-5">
+                Don&apos;t have an account?{" "}
+                <Link
+                  href="/register"
+                  className="text-blue-600 hover:underline"
+                >
+                  Sign up
+                </Link>
+              </p>
+              <Separator />
+              <p className="text-center text-gray-500 text-[15px] my-5">
+                Click here to auto-fill your email and password
+              </p>
+              <div className="flex flex-col gap-4 md:flex-row">
+                <button
+                  onClick={handleUserAutoFillButton}
+                  type="button"
+                  className="hover:cursor-pointer border border-neutral-300 px-4 flex py-[6px] gap-3 items-center justify-center font-medium rounded-full transition-all duration-300 ease-in-out hover:bg-indigo-500 hover:text-white my-4 mt-2 w-full flex-1 bg-zinc-50"
+                >
+                  User
+                </button>
+                <button
+                  onClick={handleAdminAutoFillButton}
+                  type="button"
+                  className="hover:cursor-pointer border border-neutral-300 px-4 flex py-[6px] gap-3 items-center justify-center font-medium rounded-full transition-all duration-300 ease-in-out hover:bg-indigo-500 hover:text-white my-4 mt-2 w-full flex-1 bg-zinc-50"
+                >
+                  Admin
+                </button>
+              </div>
+            </FormProvider>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
